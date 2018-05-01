c486cdf5188a27446d3678f3d27364e2af8b81ae
logeshchandrasekaran <37336302+logeshchandrasekaran@users.noreply.github.com>
2018-05-01 10:20:55 +0530

    Update selectWithCondition.sql

--- beginner/selectWithCondition.sql
+++ beginner/selectWithCondition.sql
@@ -2,4 +2,5 @@ create database person;
 use person;
 create table details(first_name varchar(50), second_name varchar(50), age int);
 insert into details values("pradeep","kumar",20);
-select * from details where age=20 and second_name="kumar";
+insert into details values("deepak","kumar",20);
+select * from details where age=20 and first_name="kumar";
